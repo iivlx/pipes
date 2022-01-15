46ccd154f4d60b5642b826e92b75a316dd9b819c
iivlx <iivlx@iivlx.com>
2022-01-14 20:41:22 -0800

    Add unicode characters for drawing pipes in console.

---
+++ include/pipe_characters.h
@@ -0,0 +1,19 @@
+#pragma once
+
+const int STRAIGHT_H = 0x2500;
+const int STRAIGHT_V = 0x2502;
+
+const int ELBOW_UR = 0x2514;
+const int ELBOW_UL = 0x2518;
+const int ELBOW_DR = 0x250c;
+const int ELBOW_DL = 0x2510;
+
+const int T_U = 0x252c;
+const int T_R = 0x2524;
+const int T_D = 0x2534;
+const int T_L = 0x251c;
+
+const int END_U = 0x2579;
+const int END_R = 0x257a;
+const int END_D = 0x257b;
+const int END_L = 0x2578;
