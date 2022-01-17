b53d42482d545f71f75a60e7480a9d87323aaf7b
iivlx <iivlx@iivlx.com>
2022-01-16 21:42:28 -0800

    Moved Connections struct from PipeCell.h to directions.h.

--- include/PipeCell.h
+++ include/PipeCell.h
@@ -3,18 +3,13 @@
 #include <tuple>
 #include <vector>
 
+#include "directions.h"
+
 enum PipeType {
   PIPE_EMPTY = 0,
   PIPE_END = 1,
   PIPE_SOURCE = 2,
   PIPE_CONNECTOR = 3,
-};
-
-struct Connections {
-  int up = 0;
-  int right = 0;
-  int down = 0;
-  int left = 0;
 };
 
 class PipeCell {
