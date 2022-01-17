fc9a0c4d410f6261fde1ec34360a855fb99a9561
iivlx <iivlx@iivlx.com>
2022-01-17 11:11:28 -0800

    Add specific directions in directions.h.

--- include/directions.h
+++ include/directions.h
@@ -20,5 +20,24 @@ struct Point {
 	int y = 0;
 };
 
+const Connections Up = Connections{ 1,0,0,0 };
+const Connections UpRight = Connections{ 1, 1, 0, 0 };
+const Connections UpDown = Connections{ 1, 0, 1, 0 };
+const Connections UpLeft = Connections{ 1, 0, 0, 1 };
+const Connections UpRightDown = Connections{ 1, 1, 1, 0 };
+const Connections UpRightDownLeft = Connections{ 1, 1, 1, 1 };
+const Connections UpRightLeft = Connections{ 1, 1, 0, 1 };
+const Connections UpDownLeft = Connections{ 1, 0, 1, 1 };
+
+const Connections Right = Connections{ 0,1,0,0 };
+const Connections RightDown = Connections{ 0,1,1,0 };
+const Connections RightLeft = Connections{ 0,1,0,1 };
+const Connections RightDownLeft = Connections{ 0,1,1,1 };
+
+const Connections Down = Connections{ 0,0,1,0 };
+const Connections DownLeft = Connections{ 0,0,1,1 };
+
+const Connections Left = Connections{ 0,0,0,1 };
+
 int countConnections(Connections c);
 int getRandomSide(Connections c);
