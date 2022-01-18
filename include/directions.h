02def0a108e41cbe3566c0da6595ef745afaa789
iivlx <iivlx@iivlx.com>
2022-01-18 00:32:21 -0800

    Fix whitespace.

--- include/directions.h
+++ include/directions.h
@@ -1,23 +1,23 @@
 #pragma once
 
 enum Directions {
-	NO_DIR = -1,
-	UP = 0,
-	RIGHT = 1,
-	DOWN = 2,
-	LEFT = 3,
+  NO_DIR = -1,
+  UP = 0,
+  RIGHT = 1,
+  DOWN = 2,
+  LEFT = 3,
 };
 
 struct Connections {
-	int up = 0;
-	int right = 0;
-	int down = 0;
-	int left = 0;
+  int up = 0;
+  int right = 0;
+  int down = 0;
+  int left = 0;
 };
 
 struct Point {
-	int x = 0;
-	int y = 0;
+  int x = 0;
+  int y = 0;
 };
 
 const Connections Up = Connections{ 1,0,0,0 };
