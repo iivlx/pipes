a91b7edb5bb996dcfaec49f2b35e938dafc19a96
iivlx <iivlx@iivlx.com>
2022-06-25 19:10:07 -0700

    Compile different gui interfaces.

---
+++ src/gui/opengl/gui_opengl.cpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glew/glew.h>
+#include <GL/GLU.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+#include "PipeGrid.h"
+#include "PipeCell.h"
+#include "PipeGridSolver.h"
+#include "pipe_characters.h"
+#include "generator.h"
+
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
